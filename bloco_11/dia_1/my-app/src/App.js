3d6ae815df416ac97ec43a21c2badf1d52f67e9c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-04-27 18:09:28 -0300

    executado exercicio bloco 11 dia 1

--- bloco_11/dia_1/my-app/src/App.js
+++ bloco_11/dia_1/my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const tarefas = ["Acordar", "Tomar café", "Escovar os dentes", "Ir trabalhar"];
+const tarefas = ["Acordar", "Tomar café", "Ir para a academia", "Escovar os dentes", "Ir trabalhar"];
 
 const App = () => ( <ul>{ tarefas.map(tarefa => <li>{ tarefa }</li>) }</ul> );
 
